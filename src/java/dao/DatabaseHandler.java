9d4c0168677c63ab8c04611959227c2b1bc33197
abdoawd <abdoawd@users.noreply.github.com>
2019-02-13 23:54:45 +0200

    db constants ,working on login

--- src/java/dao/DatabaseHandler.java
+++ src/java/dao/DatabaseHandler.java
@@ -1,17 +1,11 @@
 package dao;
 
-
-
+import beans.User;
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import utility.Constants;
 
-/**
- * @author Mohamed Ramadan
- * @author Amer Shaker
- */
 public class DatabaseHandler {
 
     private String ipAddress;
@@ -20,41 +14,30 @@ public class DatabaseHandler {
     private String userName;
     private String password;
     private boolean autoCommit;
-     private Connection connection;
+    private Connection connection;
     private Statement statement;
-//establish connection to db method 
-    public  void establishConnection() {
+
+    // Connection and Statement for creating queries
+    public DatabaseHandler() {
+        connection = establishConnection();
+
+    }
+
+    //establish connection to db method 
+    public Connection establishConnection() {
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
-                        connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", Constants.DATABASE_NAME, Constants.DATABASE_PASSWORD);
+            connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", Constants.DATABASE_NAME, Constants.DATABASE_PASSWORD);
 
-              connection.setAutoCommit(true);
+            connection.setAutoCommit(true);
             statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+
+        } catch (SQLException ex) {
         } catch (ClassNotFoundException ex) {
         }
-        catch (SQLException ex) {
-        }
-          
+        return connection;
+
     }
-    // Connection and Statement for creating queries
-
-
-    public DatabaseHandler(String databaseName, String userName, String password) {
-        this( databaseName, userName, password, true);
-    }
-
-    public DatabaseHandler( String databaseName, String userName, String password, boolean autoCommit) {
-        
-        this.databaseName = databaseName;
-        this.userName = userName;
-        this.password = password;
-        this.autoCommit = autoCommit;
-
-        // Initialize database connection and create a Statement object
-        establishConnection();
-    }
-
-  
 
     // Close the connection
     public void closeConnection() throws SQLException {
@@ -62,35 +45,59 @@ public class DatabaseHandler {
         connection.close();
     }
 
-    public void login(String emailAddress, String password) throws SQLException {
-       
-        
+    public int testConnection() {
+        int i = 0;
+        try {
+            ResultSet rs = null;
+            PreparedStatement s = connection.prepareStatement("select * from " + Constants.USER_TABLE_NAME);
+            rs = s.executeQuery();
+            while (rs.next()) {
+                i = 1;
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return i;
+    }
+
+    public User login(String emailAddress, String password) {
+        try {
+            ResultSet rs = null;
+            PreparedStatement s = connection.prepareStatement("select * from " + Constants.USER_TABLE_NAME
+                    + " where ");
+
+            rs = s.executeQuery();
+            while (rs.next()) {
+
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return null;
     }
 
     public boolean sinUp(String emailAddress, String userName, String password, String ipAddressString) throws SQLException {
-       return false ;
+        return false;
     }
 
     public boolean deleteUser(String emailAddress) throws SQLException {
-       return false ;
+        return false;
     }
-
-   
 
     public void getAllUsers() throws SQLException {
-       
+
     }
+
     public void getAllProduct() throws SQLException {
-       
+
     }
 
     boolean updateColumn(String emailAddress, String columnName, String columnValue) throws SQLException {
-       return false ;
+        return false;
     }
 
     boolean updateColumn(String emailAddress, String columnName, int columnValue) throws SQLException {
-        return false ;
+        return false;
     }
-    
 
 }
